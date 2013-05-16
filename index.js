060888c6315db3c71606f57966744b6f0aadc30a
jkroso <jkroso@gmail.com>
2013-05-16 20:50:23 +1200

    move callback out of try catch

--- index.js
+++ index.js
@@ -13,19 +13,18 @@ module.exports = function(username, repo, fn) {
       'user-agent': 'https://npmjs.org/package/github-latest'
     }
   }, function(res) {
-
     var data = '';
+    
     res.on('end', function() {
       try {
         var tags = JSON.parse(data).map(function(item){
           return item.ref.split('/').pop();
         })
         tags.sort(semver.rcompare);
-
-        fn(null, tags[0]);
       } catch (e) {
         fn(e);
       }
+      fn(null, tags[0]);
     });
 
     res.on('data', function(chunk) {
