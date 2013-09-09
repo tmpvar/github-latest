cf01f911659ccaf748db59920b1835c18b065dfe
Jake Rosoman <jkroso@gmail.com>
2013-09-10 11:14:10 +1200

    rm chai.js

--- test/github-latest.test.js
+++ test/github-latest.test.js
@@ -1,18 +1,18 @@
 
-var chai = require('./chai')
-  , latest = require('..')
+var assert = require('assert')
+var latest = require('..')
 
 describe('github-latest', function(){
-	it('should work', function(done){
-		this.timeout(10000)
-		var pending = 10
-		var i = 0
-		for (var i = 0; i < pending; i++) {
-			latest('jkroso', 'result-type', function(e, tag){
-				if (e) return done(e)
-				tag.should.equal('1.0.0')
-				if (--pending === 0) done()
-			})
-		}
-	})
+  it('should work', function(done){
+    this.timeout(10000)
+    var pending = 10
+    var i = 0
+    for (var i = 0; i < pending; i++) {
+      latest('jkroso', 'result-type', function(e, tag){
+        if (e) return done(e)
+        assert(tag == '1.0.0')
+        if (--pending === 0) done()
+      })
+    }
+  })
 })
