d11b0cf8da73cf9447d2537bb025a006ecdb1641
Jake Rosoman <jkroso@gmail.com>
2013-09-10 10:57:34 +1200

    update API's

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var semver = require('semver')
-  , https = require('https')
+var https = require('https')
 
 var username = process.env.GITHUB_USERNAME
 var password = process.env.GITHUB_PASSWORD
@@ -15,15 +15,10 @@ function encode(str){
   return new Buffer(str).toString('base64')
 }
 
-module.exports = function(username, repo, fn) {
-  var path = ''
-    + '/repos/' + username
-    + '/' + repo
-    + '/git/refs/tags/'
-
+module.exports = function(user, repo, fn) {
   https.request({
     host: 'api.github.com',
-    path: path,
+    path: '/repos/' + user + '/' + repo + '/tags',
     port: 443,
     method: 'GET',
     headers: headers,
@@ -32,25 +27,24 @@ module.exports = function(username, repo, fn) {
     var data = ''
 
     // data has to be read
-    res.on('data', function(chunk) {
-      data += chunk
-    });
+    res.on('readable', function(chunk){
+      data += this.read() || ''
+    })
 
-    if(!(/^200/).test(res.headers.status))
+    if(res.statusCode != 200) {
       return fn(new Error(res.headers.status))
+    }
 
-    res.on('end', function() {
+    res.on('end', function(){
       try {
-        var tags = JSON.parse(data)
-          .map(function(item){
-            return item.ref.split('/').pop()
-          })
-          .filter(semver.valid)
-          .sort(semver.rcompare)
+        data = JSON.parse(data)
+        .map(function(item){ return item.name })
+        .filter(semver.valid)
+        .sort(semver.rcompare)
       } catch (e) {
         return fn(e)
       }
-      fn(null, tags[0])
+      fn(null, data[0])
     })
   })
   .on('error', fn)
